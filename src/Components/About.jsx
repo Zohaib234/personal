b124d833e7f59c685966cd3d39d4a3b6aeb4bcef
Zohaib Khoso <101723080+Zohaib234@users.noreply.github.com>
2023-06-01 08:22:39 +0000

    skills added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -31,12 +31,12 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Backend Developer ",
+  "Flutter",
+  "Express js ",
+  "React Js",
+  "Javascript ",
+  "Firebase",
 ];
 
 /**
